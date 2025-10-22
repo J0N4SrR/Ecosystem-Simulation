16464abdf2d0757c4db5b18ccebc6ecf4699011d
Jonas Ribeiro <krnsjrr@gmail.com.br>
2025-10-22 13:00:40 -0300

    criando a classe Creature

---
+++ src/main/java/rosa/ribeiro/jonas/Creature.java
@@ -0,0 +1,9 @@
+package rosa.ribeiro.jonas;
+
+public class Creature {
+    private int hp;
+    private int hungry;
+    private int stamina;
+    private int thirst;
+    private int[] position;
+}
